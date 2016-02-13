337d0db782cf4e2e300d8613a270e92020ba44e4
John Wood <johnrobertwood@yahoo.com>
2016-02-12 23:28:51 -0800

    Adding table styling, edit and delete mode and pseudocode for app script

---
+++ app.js
@@ -0,0 +1,15 @@
+//Get the food and quantity inputs and assign them to variables
+//Assign the button to a variable
+//Assign the parent elemnt in the table to variable
+
+//Create an add row function that builds up a table row by appending data cells with each column value
+
+//Create an edit function that selects the parent node of the button
+//then swaps the p value with the input value if you click edit
+//Clicking edit again to switch out of editmode swaps edit input value with p value
+
+//Create delete function that selects the parent row node of the button to a variable, then 
+//moves up to the next parent and removes that variable which is now the child
+
+//Create a bind function that assigns click event handlers to the edit and delete functions
+//use a for loop to go through each table row calling the bind function
